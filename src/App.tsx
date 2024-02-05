33d0aedbaf25f58484563cbd04a09d6c530d2dd3
alekcandr1 <alekcandrmain@gmail.com>
2024-02-05 18:20:25 +0300

    add removeTask function

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import './App.css';
 import { TodoList } from './components/TodoList';
 
-let list1 = [
-    {id: 1, title: "HTML&CSS", isDone: true},
-    {id: 2, title: "JS", isDone: true},
-    {id: 3, title: "React", isDone: false},
-]
-let list2 = [
-    {id: 1, title: "Jentelmens of fortune", isDone: true},
-    {id: 2, title: "Terminator", isDone: false},
-    {id: 3, title: "Terminator 2", isDone: true},
-]
 
 function App() {
+    let list1 = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "React", isDone: false },
+    ]
+    let list2 = [
+        { id: 1, title: "Jentelmens of fortune", isDone: true },
+        { id: 2, title: "Terminator", isDone: false },
+        { id: 3, title: "Terminator 2", isDone: true },
+    ]
+
+    function removeTask() {
+
+    }
 
     return (
         <div className="App">
